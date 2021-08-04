4db3fefb601a486634fcc6f5a5fd3918f57d25c7
ivy-Sunny <thelife_dream@icloud.com>
2021-08-04 10:47:57 +0800

    Thread ---> Stop, Sleep, Yield, Join

---
+++ Thread/src/main/java/com/ivy/thread/TestDaemon.java
@@ -0,0 +1,50 @@
+package com.ivy.thread;
+
+/**
+ * TestDeamon
+ * 守护进程
+ *
+ * @Author: ivy
+ * @CreateTime: 2021-08-04
+ */
+
+public class TestDaemon {
+    public static void main(String[] args) {
+        God god = new God();
+        IvySunny ivySunny = new IvySunny();
+        Thread thread = new Thread(god);
+        //默认是false，表示是用户线程，正常的线程都是用户线程
+        thread.setDaemon(true);
+        //上帝守护线程启动
+        thread.start();
+
+        //ivySunny用户线程启动
+        new Thread(ivySunny).start();
+    }
+}
+
+/**
+ * 我
+ */
+class IvySunny implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 36500; i++) {
+            System.out.println("一生都要开心的活着");
+        }
+        System.out.println("goodbye! world");
+    }
+}
+
+/**
+ * 上帝
+ */
+class God implements Runnable {
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("上帝守护着你...");
+        }
+    }
+}
---
+++ Thread/src/main/java/com/ivy/thread/TestState.java
@@ -0,0 +1,40 @@
+package com.ivy.thread;
+
+/**
+ * TestState
+ *
+ * @Author: ivy
+ * @CreateTime: 2021-08-04
+ */
+public class TestState {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("----------------");
+        });
+
+        //观察状态
+        Thread.State state = thread.getState();
+        System.out.println(state);
+
+        //观察启动后
+        thread.start();
+        state = thread.getState();
+        //run
+        System.out.println(state);
+
+        //只要线程不终止，就一直输出状态
+        while (state != Thread.State.TERMINATED){
+            Thread.sleep(250);
+            //更新线程状态
+            state = thread.getState();
+            System.out.println(state);
+        }
+    }
+}
