173c3714bdfa94e5af805c54575191ab4e56d5a9
ivy-Sunny <thelife_dream@icloud.com>
2021-08-05 10:22:59 +0800

    annotation & reflect

---
+++ annotationAndReflect/src/main/java/com/ivy/annotationandreflect/AnnotationAndReflectApplication.java
@@ -0,0 +1,13 @@
+package com.ivy.annotationandreflect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationAndReflectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationAndReflectApplication.class, args);
+    }
+
+}
---
+++ annotationAndReflect/src/test/java/com/ivy/annotationandreflect/AnnotationAndReflectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivy.annotationandreflect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationAndReflectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
