8295637c7628849597cce347c851669f0bf0a679
ivySunny <thelife_dream@icloud.com>
2021-08-09 19:50:53 +0800

    JDBC

---
+++ JDBC/src/test/java/com/ivy/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivy.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
