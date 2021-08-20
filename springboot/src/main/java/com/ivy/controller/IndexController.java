3f55e507fd837b8a98f92dfd8565e17e6d5b8f80
ivySunny <thelife_dream@icloud.com>
2021-08-20 14:57:29 +0800

    SpringBoot

---
+++ springboot/src/main/java/com/ivy/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.ivy.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController{
+    //@RequestMapping({"/","/index.html"})
+    //public String index() {
+    //    return "index";
+    //}
+}
