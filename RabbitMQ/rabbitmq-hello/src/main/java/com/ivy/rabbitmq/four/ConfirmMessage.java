ef7fa976a1df87d8fed0913caa93f6dac7e557e7
ivySunny <thelife_dream@icloud.com>
2021-08-23 21:18:42 +0800

    RabbitMQ publishMessageAsync

--- RabbitMQ/rabbitmq-hello/src/main/java/com/ivy/rabbitmq/four/ConfirmMessage.java
+++ RabbitMQ/rabbitmq-hello/src/main/java/com/ivy/rabbitmq/four/ConfirmMessage.java
@@ -2,6 +2,7 @@ package com.ivy.rabbitmq.four;
 
 import com.ivy.rabbitmq.RabbitMqUtils;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmCallback;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -43,7 +44,7 @@ public class ConfirmMessage {
             }
         }
         //结束时间
-        System.out.println("发布" + MESSAGE_COUNT + "个单独确认消息，耗时" + (System.currentTimeMillis() - starttime) + "ms");
+        System.out.println("发布" + MESSAGE_COUNT + "个单独发布确认消息，耗时" + (System.currentTimeMillis() - starttime) + "ms");
     }
 
     /**
@@ -63,13 +64,55 @@ public class ConfirmMessage {
         for (int i = 0; i < MESSAGE_COUNT; i++) {
             String message = "" + i;
             channel.basicPublish("", queueName, null, message.getBytes());
-            if (i%batchSize == 0){
+            if (i % batchSize == 0) {
                 channel.waitForConfirms();
             }
         }
         //发布确认
         channel.waitForConfirms();
         //结束时间
-        System.out.println("发布" + MESSAGE_COUNT + "个批量确认消息，耗时" + (System.currentTimeMillis() - starttime) + "ms");
+        System.out.println("发布" + MESSAGE_COUNT + "个批量发布确认消息，耗时" + (System.currentTimeMillis() - starttime) + "ms");
+    }
+
+    /**
+     * 异步确认发布
+     */
+    public static void publishMessageAsync() throws IOException, InterruptedException {
+        Channel channel = RabbitMqUtils.getChannel();
+        //队列的声明
+        String queueName = UUID.randomUUID().toString();
+        channel.queueDeclare(queueName, false, false, false, null);
+        //开启发布确认
+        channel.confirmSelect();
+        /**
+         * 消息确认成功 回调函数
+         */
+        ConfirmCallback ackCallback = (deliveryTag, multiple) -> {
+            System.out.println("确认的消息:" + deliveryTag);
+        };
+        /**
+         * 消息确认失败 回调函数
+         *  params:
+         *      1.消息的标记
+         *      2.是否为批量确认
+         */
+        ConfirmCallback nackCallback = (deliveryTag, multiple) -> {
+            System.out.println("未确认的消息:" + deliveryTag);
+        };
+        /**
+         * 准备消息的监听器，监听哪些消息失败，哪些消息成功 (异步)
+         * 单参 监听成功消息
+         * 多参 监听成功，失败消息
+         */
+        channel.addConfirmListener(ackCallback, nackCallback);
+        //开始时间
+        long starttime = System.currentTimeMillis();
+        for (int i = 0; i < MESSAGE_COUNT; i++) {
+            String message = "" + i;
+            channel.basicPublish("", queueName, null, message.getBytes());
+        }
+
+        //结束时间
+        System.out.println("发布" + MESSAGE_COUNT + "个异步发布确认消息，耗时" + (System.currentTimeMillis() - starttime) + "ms");
     }
 }
