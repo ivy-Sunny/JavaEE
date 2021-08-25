849fdcf6ba372c8237fec98ddd1b3e9cb6365085
ivySunny <thelife_dream@icloud.com>
2021-08-25 17:22:46 +0800

    Eureka CLient

--- SpringCloud/springcloud-provider-dept-8001/src/main/java/com/ivy/springcloud/DeptProvider_8001.java
+++ SpringCloud/springcloud-provider-dept-8001/src/main/java/com/ivy/springcloud/DeptProvider_8001.java
@@ -2,9 +2,11 @@ package com.ivy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class, args);
