c8edf7c352d8f1d1caf3973bf1ca1310d326d3fd
ivySunny <thelife_dream@icloud.com>
2021-08-26 16:41:46 +0800

    SpringCloud Hystrix

--- SpringCloud/springcloud-provider-dept-8004-hystrix/src/main/java/com/ivy/springcloud/DeptProvider_8004_Hystrix.java
+++ SpringCloud/springcloud-provider-dept-8004-hystrix/src/main/java/com/ivy/springcloud/DeptProvider_8004_Hystrix.java
@@ -2,6 +2,7 @@ package com.ivy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -9,6 +10,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 //服务发现
 @EnableDiscoveryClient
+//服务熔断
+@EnableCircuitBreaker
 public class DeptProvider_8004_Hystrix {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8004_Hystrix.class, args);
--- SpringCloud/springcloud-provider-dept-8004-hystrix/src/main/java/com/ivy/springcloud/controller/DeptController.java
+++ SpringCloud/springcloud-provider-dept-8004-hystrix/src/main/java/com/ivy/springcloud/controller/DeptController.java
@@ -2,58 +2,36 @@ package com.ivy.springcloud.controller;
 
 import com.ivy.springcloud.pojo.Dept;
 import com.ivy.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class DeptController {
 
     @Autowired
     private DeptService deptService;
-    @Autowired
-    private DiscoveryClient client;
 
-    @PostMapping("/dept/add")
-    public boolean addDept(Dept dept) {
-        return deptService.addDept(dept);
-    }
-
+    @HystrixCommand(fallbackMethod = "hystrixGet")
     @GetMapping("/dept/get/{id}")
-    public Dept get(@PathVariable int id) {
-        return deptService.queryById(id);
-    }
-
-    @GetMapping("/dept/list")
-    public List<Dept> queryAll() {
-        return deptService.queryAll();
+    public Dept get(@PathVariable("id") int id) {
+        Dept dept = deptService.queryById(id);
+        if (dept == null) {
+            throw new RuntimeException("id => " + id + "，不存在该用户或者信息无法找到……");
+        }
+        return dept;
     }
 
     /**
-     * 注册进来的微服务～，获取一些消息
+     * 备选方案
      */
-    @GetMapping("/dept/discovery")
-    public Object discovery() {
-        //获得微服务列表的清单
-        List<String> services = client.getServices();
-        System.out.println("discovery => services:" + services);
-
-        //通过微服务id（applicationName）获取一个具体的微服务信息
-        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
-        for (ServiceInstance instance : instances) {
-            System.out.println(
-                    instance.getHost() + "\t" +
-                            instance.getPort() + "\t" +
-                            instance.getUri() + "\t" +
-                            instance.getServiceId()
-            );
-        }
-        return this.client;
+    public Dept hystrixGet(@PathVariable("id") int id) {
+        return new Dept()
+                .setDeptno(id)
+                .setDname("id => " + id + "，不存在该用户或者信息无法找到……")
+                .setDb_source("no this database in MySql");
     }
+
 }
